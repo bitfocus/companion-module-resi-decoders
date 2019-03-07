38dccbf7af152f816a2869430044ff364f81493b
Jake Jones <jake@thejoneses.family>
2019-03-07 10:34:41 -0600

    updated files list

---
+++ HELP.md
@@ -0,0 +1,10 @@
+## Living As One Decoders
+
+This module uses Rosstalk to communicate with Living As One Decoders
+
+Enter the IP address of the decoder when setting up the instance.
+
+**Available commands**
+
+* Play
+* Pause
---
+++ index.js
@@ -0,0 +1,145 @@
+var tcp = require('../../tcp');
+var instance_skel = require('../../instance_skel');
+var debug;
+var log;
+
+function instance(system, id, config) {
+	var self = this;
+
+	// super-constructor
+	instance_skel.apply(this, arguments);
+
+	self.actions(); // export actions
+
+	return self;
+}
+
+instance.prototype.updateConfig = function(config) {
+	var self = this;
+
+	self.config = config;
+	self.init_tcp();
+};
+
+instance.prototype.init = function() {
+	var self = this;
+
+	debug = self.debug;
+	log = self.log;
+
+	self.status(self.STATE_UNKNOWN);
+
+	self.init_tcp();
+};
+
+instance.prototype.init_tcp = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+		delete self.socket;
+	}
+
+	if (self.config.host) {
+		self.socket = new tcp(self.config.host, 7788);
+
+		self.socket.on('status_change', function (status, message) {
+			self.status(status, message);
+		});
+
+		self.socket.on('error', function (err) {
+			debug("Network error", err);
+			self.status(self.STATE_ERROR, err);
+			self.log('error',"Network error: " + err.message);
+		});
+
+		self.socket.on('connect', function () {
+			self.status(self.STATE_OK);
+			debug("Connected");
+		})
+
+		self.socket.on('data', function (data) {});
+	}
+};
+
+// Return config fields for web config
+instance.prototype.config_fields = function () {
+	var self = this;
+	return [
+		{
+			type: 'text',
+			id: 'info',
+			width: 12,
+			label: 'Information',
+			value: 'This module uses Rosstalk for communication with Living As One decoders.'
+		},
+		{
+			type: 'textinput',
+			id: 'host',
+			label: 'LA1 Decoder IP',
+			width: 6,
+			regex: self.REGEX_IP
+		},
+	]
+};
+
+// When module gets deleted
+instance.prototype.destroy = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+	}
+
+	debug("destroy", self.id);;
+};
+
+
+instance.prototype.actions = function(system) {
+	var self = this;
+	self.system.emit('instance_actions', self.id, {
+
+		'play': { label: 'Play' },
+		'pause': { label: 'Pause' },
+
+	});
+}
+
+instance.prototype.action = function(action) {
+	var self = this;
+	var id = action.action;
+	var opt = action.options;
+
+	// parseInt(action.options.int)
+	var cmd;
+
+	switch (action.action) {
+
+		case 'play':
+			cmd = 'CC ' + "1:1";
+			break;
+		case 'pause':
+			cmd = 'CC ' + "1:2";
+			break;
+	}
+
+	if (cmd !== undefined) {
+
+		debug('sending tcp',cmd,"to",self.config.host);
+
+		if (self.socket !== undefined && self.socket.connected) {
+			self.socket.send(cmd + "\n");
+		}
+		else {
+			debug('Socket not connected :(');
+		}
+
+	}
+
+	debug('action():', action);
+
+
+};
+
+instance_skel.extendedBy(instance);
+exports = module.exports = instance;
